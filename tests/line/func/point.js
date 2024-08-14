95b70d0dd570f6adc1c5e65926ef460bfa261adc
Assayyaad <hi@thealpha.pro>
2024-08-14 12:42:28 +0800

    tests(line): إعادة كتابة ملفات الإختبار

--- tests/line/func/point.js
+++ tests/line/func/point.js
@@ -1,230 +1,213 @@
 import { equal } from 'assert/strict'
 import { Line } from '../../../src/exports.js'
+import { ok } from 'assert'
 
-let line
-let current
+const posLine = Line.create.one({ end: +10 })
+const negLine = Line.create.one({ end: -10 })
+const posCurrent = +5
+const negCurrent = -5
 
 export default function () {
-  describe('1: تحقق مما إذا كانت القيمة داخل المستقيم الموجب', function () {
-    before(function () {
-      line = Line.create.one({ dis: 10 })
+  describe('inside', function () {
+    it('1: داخل المستقيم', function () {
+      // [0 -> 0 -> 10]
+      ok(Line.point.inside(posLine, 0))
+
+      // [0 -> 5 -> 10]
+      ok(Line.point.inside(posLine, 5))
+
+      // [0 -> 10 -> 10]
+      ok(Line.point.inside(posLine, 10))
+
+      // [0 -> 0 -> -10]
+      ok(Line.point.inside(negLine, 0))
+
+      // [0 -> -5 -> -10]
+      ok(Line.point.inside(negLine, -5))
+
+      // [0 -> -10 -> -10]
+      ok(Line.point.inside(negLine, -10))
     })
 
-    it('1.1: خارج المستقيم - قبل', function () {
-      // -1 <-> [0 <-> 10]
+    it('1: خارج المستقيم', function () {
+      // -1 -> [0 -> 10]
+      ok(!Line.point.inside(posLine, -1))
 
-      const a = Line.point.inside(line, -1)
-      equal(a, false)
-    })
+      // [0 -> 10] -> 11
+      ok(!Line.point.inside(posLine, 11))
 
-    it('1.2: خارج المستقيم - بعد', function () {
-      // [0 <-> 10] <-> 11
+      // 1 -> [0 -> -10]
+      ok(!Line.point.inside(negLine, 1))
 
-      const a = Line.point.inside(line, 11)
-      equal(a, false)
-    })
-
-    it('1.3: داخل المستقيم', function () {
-      // [0 <-> 5 <-> 10]
-
-      const a = Line.point.inside(line, 5)
-      equal(a, true)
-    })
-
-    it('1.4: على بداية المستقيم', function () {
-      // [0' <-> 10]
-
-      const a = Line.point.inside(line, 0)
-      equal(a, true)
-    })
-
-    it('1.5: على نهاية المستقيم', function () {
-      // [0 <-> '10]
-
-      const a = Line.point.inside(line, 10)
-      equal(a, true)
+      // [0 -> -10] -> -11
+      ok(!Line.point.inside(negLine, -11))
     })
   })
 
-  describe('2: تحقق مما إذا كانت القيمة داخل المستقيم السالب', function () {
-    before(function () {
-      line = Line.create.one({ dis: 10, neg: true })
+  describe('move', function () {
+    afterEach(function () {
+      posLine.start = 0
+      posLine.end = 10
+      negLine.start = 0
+      negLine.end = -10
     })
 
-    it('2.1: خارج المستقيم - قبل', function () {
-      // -11 <-> [-10 <-> 0]
+    it('1: عكس المستقيم', function () {
+      // [0 -> 10] => [10 -> 0]
 
-      const a = Line.point.inside(line, -11)
-      equal(a, false)
+      let l = Line.point.move.flip(posLine)
+      equal(l.start, 10)
+      equal(l.end, 0)
+
+      // [0 -> -10] => [-10 -> 0]
+
+      l = Line.point.move.flip(negLine)
+      equal(l.start, -10)
+      equal(l.end, 0)
     })
 
-    it('2.2: خارج المستقيم - بعد', function () {
-      // [-10 <-> 0] <-> 1
+    it('2: تحديث موضع البداية فقط', function () {
+      // [0 -> 10] => [-5 -> 10]
 
-      const a = Line.point.inside(line, 1)
-      equal(a, false)
+      let l = Line.point.move(posLine, -5, { type: 'by', target: 'start' })
+
+      equal(l.start, -5)
+      equal(l.end, 10)
+
+      // [-5 -> 10] => [20 -> 10]
+
+      l = Line.point.move(l, 20, { type: 'to', target: 'start' })
+      equal(l.start, 20)
+      equal(l.end, 10)
+
+      // [0 -> -10] => [5 -> -10]
+
+      l = Line.point.move(negLine, 5, { type: 'by', target: 'start' })
+
+      equal(l.start, 5)
+      equal(l.end, -10)
+
+      // [5 -> -10] => [-20 -> -10]
+
+      l = Line.point.move(l, -20, { type: 'to', target: 'start' })
+      equal(l.start, -20)
+      equal(l.end, -10)
     })
 
-    it('2.3: داخل المستقيم', function () {
-      // [-10 <-> -5 <-> 0]
+    it('3: تحديث موضع النهاية فقط', function () {
+      // [0 -> 10] => [0 -> 5]
 
-      const a = Line.point.inside(line, -5)
-      equal(a, true)
+      let l = Line.point.move(posLine, -5, { type: 'by', target: 'end' })
+      equal(l.start, 0)
+      equal(l.end, 5)
+
+      // [0 -> 5] => [0 -> 20]
+
+      l = Line.point.move(l, 20, { type: 'to', target: 'end' })
+      equal(l.start, 0)
+      equal(l.end, 20)
+
+      // [0 -> -10] => [0 -> -5]
+
+      l = Line.point.move(negLine, 5, { type: 'by', target: 'end' })
+      equal(l.start, 0)
+      equal(l.end, -5)
+
+      // [0 -> -5] => [0 -> -20]
+
+      l = Line.point.move(l, -20, { type: 'to', target: 'end' })
+      equal(l.start, 0)
+      equal(l.end, -20)
     })
 
-    it('2.4: على بداية المستقيم', function () {
-      // [-10 <-> '0]
+    it('4: تحديث موضع البداية والنهاية معاً', function () {
+      // [0 -> 10] => [-5 -> 5]
 
-      const a = Line.point.inside(line, 0)
-      equal(a, true)
-    })
+      let l = Line.point.move(posLine, -5, { type: 'by', target: 'both' })
+      equal(l.start, -5)
+      equal(l.end, 5)
 
-    it('2.5: على نهاية المستقيم', function () {
-      // [-10' <-> 0]
+      // [-5 -> 5] => [20 -> 20]
 
-      const a = Line.point.inside(line, -10)
-      equal(a, true)
-    })
-  })
+      l = Line.point.move(l, 20, { type: 'to', target: 'both' })
+      equal(l.start, 20)
+      equal(l.end, 20)
 
-  describe('3: حرّك القيمة على المستقيم الموجب', function () {
-    before(function () {
-      line = Line.create.one({ dis: 10 })
-      current = 5
-    })
+      // [0 -> -10] => [5 -> -5]
 
-    it('3.1: خطوة صفر', function () {
-      // [0 <- 5 -> 10] 0>> = [0 <- 5 -> 10]
+      l = Line.point.move(negLine, 5, { type: 'by', target: 'both' })
+      equal(l.start, 5)
+      equal(l.end, -5)
 
-      const a = Line.point.ride(line, current, 0)
-      equal(a, 5)
-    })
+      // [5 -> -5] => [-20 -> -20]
 
-    it('3.2: الحركة للأمام داخل النطاق', function () {
-      // [0 <- 5 -> 10] 3>> = [0 <- 8 -> 10]
-
-      const a = Line.point.ride(line, current, 3)
-      equal(a, 8)
-    })
-
-    it('3.3: عدم تجاوز الحد الأقصى', function () {
-      // [0 <- 5 -> 10] 10>> = [0 <-> '10]
-
-      const a = Line.point.ride(line, current, 10)
-      equal(a, 10)
-    })
-
-    it('3.4: الحركة للخلف داخل النطاق', function () {
-      // [0 <- 5 -> 10] <<3 = [0 <- 2 -> 10]
-
-      const a = Line.point.ride(line, current, 3, true)
-      equal(a, 2)
-    })
-
-    it('3.5: عدم تجاوز الحد الأدنى', function () {
-      // [0 <- 5 -> 10] <<10 = [0' <-> 10]
-
-      const a = Line.point.ride(line, current, 10, true)
-      equal(a, 0)
-    })
-  })
-
-  describe('4: حرّك القيمة على المستقيم السالب', function () {
-    before(function () {
-      line = Line.create.one({ dis: 10, neg: true })
-      current = -5
-    })
-
-    it('4.1: خطوة صفر', function () {
-      // [-10 <- -5 -> 0] <<0 = [-10 <- -5 -> 0]
-
-      const a = Line.point.ride(line, current, 0)
-      equal(a, -5)
-    })
-
-    it('4.2: الحركة للأمام داخل النطاق', function () {
-      // [-10 <- -5 -> 0] <<3 = [-10 <- -8 -> 0]
-
-      const a = Line.point.ride(line, current, 3)
-      equal(a, -8)
-    })
-
-    it('4.3: عدم تجاوز الحد الأقصى', function () {
-      // [-10 <- -5 -> 0] <<10 = [-10' <-> 0]
-
-      const a = Line.point.ride(line, current, 10)
-      equal(a, -10)
-    })
-
-    it('4.4: الحركة للخلف داخل النطاق', function () {
-      // [-10 <- -5 -> 0] 3>> = [-10 <- -2 -> 0]
-
-      const a = Line.point.ride(line, current, 3, true)
-      equal(a, -2)
-    })
-
-    it('4.5: عدم تجاوز الحد الأدنى', function () {
-      // [-10 <- -5 -> 0] 10>> = [-10 <-> '0]
-
-      const a = Line.point.ride(line, current, 10, true)
-      equal(a, 0)
+      l = Line.point.move(l, -20, { type: 'to', target: 'both' })
+      equal(l.start, -20)
+      equal(l.end, -20)
     })
   })
 
-  describe('5: تحديث مواضع المستقيم', function () {
-    beforeEach(function () {
-      line = Line.create.one({ start: 0, end: 10 })
-    })
+  describe('ride', function () {
+    describe('3: حرّك القيمة على المستقيم الموجب', function () {
+      it('1: خطوة صفر', function () {
+        // [0 -> +5 -> +10] 0>> = [0 -> +5 -> +10]
 
-    it('5.1: عكس مستقيم', function () {
-      // [0 -> 10] => [10 <- 0]
+        let p = Line.point.ride(posLine, posCurrent, 0)
+        equal(p, +5)
 
-      const a = Line.point.move.flip(line)
-      equal(a.start, 10)
-      equal(a.end, 0)
-    })
+        // [0 -> -5 -> -10] 0>> = [0 -> -5 -> -10]
 
-    it('5.2: تحديث موضع البداية فقط', function () {
-      // [0 -> 10] => [5 -> 10]
+        p = Line.point.ride(negLine, negCurrent, 0)
+        equal(p, -5)
+      })
 
-      let a = Line.point.move(line, 5, { type: 'by', target: 'start' })
+      it('2: الحركة للأمام داخل النطاق', function () {
+        // [0 -> +5 -> +10] 3>> = [0 -> +8 -> +10]
 
-      equal(a.start, 5)
-      equal(a.end, 10)
+        let p = Line.point.ride(posLine, posCurrent, 3)
+        equal(p, +8)
 
-      // [5 -> 10] => [10 <- 20]
+        // [0 -> -5 -> -10] 3>> = [0 -> -8 -> -10]
 
-      a = Line.point.move(a, 20, { type: 'to', target: 'start' })
-      equal(a.start, 20)
-      equal(a.end, 10)
-    })
+        p = Line.point.ride(negLine, negCurrent, 3)
+        equal(p, -8)
+      })
 
-    it('5.3: تحديث موضع النهاية فقط', function () {
-      // [0 -> 10] => [0 -> 15]
+      it('3: عدم تجاوز الحد الأقصى', function () {
+        // [0 -> +5 -> +10] 10>> = [0 -> +10 -> +10]
 
-      let a = Line.point.move(line, 5, { type: 'by', target: 'end' })
-      equal(a.start, 0)
-      equal(a.end, 15)
+        let p = Line.point.ride(posLine, posCurrent, 10)
+        equal(p, +10)
 
-      // [0 -> 15] => [0 -> 20]
+        // [0 -> -5 -> -10] 10>> = [0 -> -10 -> -10]
 
-      a = Line.point.move(a, 20, { type: 'to', target: 'end' })
-      equal(a.start, 0)
-      equal(a.end, 20)
-    })
+        p = Line.point.ride(negLine, negCurrent, 10)
+        equal(p, -10)
+      })
 
-    it('5.4: تحديث موضع البداية والنهاية معاً', function () {
-      // [0 -> 10] => [5 -> 15]
+      it('4: الحركة للخلف داخل النطاق', function () {
+        // [0 -> +5 -> +10] <<3 = [0 -> +2 -> +10]
 
-      let a = Line.point.move(line, 5, { type: 'by', target: 'both' })
-      equal(a.start, 5)
-      equal(a.end, 15)
+        let p = Line.point.ride(posLine, posCurrent, 3, true)
+        equal(p, +2)
 
-      // [5 -> 15] => [20 -> 20]
+        // [0 -> -5 -> -10] <<3 = [0 -> -2 -> -10]
 
-      a = Line.point.move(a, 20, { type: 'to', target: 'both' })
-      equal(a.start, 20)
-      equal(a.end, 20)
+        p = Line.point.ride(negLine, negCurrent, 3, true)
+        equal(p, -2)
+      })
+
+      it('5: عدم تجاوز الحد الأدنى', function () {
+        // [0 -> +5 -> +10] <<10 = [0 -> +0 -> +10]
+
+        let p = Line.point.ride(posLine, posCurrent, 10, true)
+        equal(p, 0)
+
+        // [0 -> -5 -> -10] <<10 = [0 -> 0 -> -10]
+
+        p = Line.point.ride(negLine, negCurrent, 10, true)
+        equal(p, 0)
+      })
     })
   })
 }
